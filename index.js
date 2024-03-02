6e5bb9d5d462d28422493eb99de94ab5584588f9
yangweiming <y1257891258@qq.com>
2024-03-03 01:32:10 +0800

    init

---
+++ index.js
@@ -0,0 +1,3 @@
+import { initProvider } from './src/starter';
+
+export { initProvider };
---
+++ src/starter.js
@@ -0,0 +1,5 @@
+import { getProvider } from "@widget-plus/utils";
+
+export const initProvider = () => {
+  return getProvider();
+}
